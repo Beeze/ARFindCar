86c7c47761667d3320298ebe354357dc7ef7bd1b
Marc <m.brown298@gmail.com>
2017-10-15 02:44:27 -0400

    Added computer vision aspects

--- ARKit+CoreLocation/ViewController.swift
+++ ARKit+CoreLocation/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 import SceneKit 
 import MapKit
+import ARKit
+import Vision
 import CocoaLumberjack
 
 @available(iOS 11.0, *)
 class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDelegate {
+    
+    let bubbleDepth : Float = 0.01 // the 'depth' of 3D text
+    var latestPrediction : String = "…" // a variable containing the latest CoreML prediction
+    
+    // COREML
+    var visionRequests = [VNRequest]()
+    let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
+    var carsScene = [String]()
+    
     let sceneLocationView = SceneLocationView()
     
     let mapView = MKMapView()
@@ -26,6 +37,8 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
     var nodesInScene:[LocationNode] = []
     
     var hasSavedLocation:Bool = false
+    
+    var learn_more_button:UIButton = UIButton()
     
     var updateUserLocationTimer: Timer?
     
@@ -49,11 +62,193 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneLocationView.autoenablesDefaultLighting = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(gestureRecognize:)))
+        sceneLocationView.addGestureRecognizer(tapGesture)
+        
+        //         Set up Vision Model
+        guard let selectedModel = try? VNCoreMLModel(for: CarRecognition().model) else { // (Optional) This can be replaced with other models on https://developer.apple.com/machine-learning/
+            fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project from https://developer.apple.com/machine-learning/ . Also ensure the model is part of a target (see: https://stackoverflow.com/questions/45884085/model-is-not-part-of-any-target-add-the-model-to-a-target-to-enable-generation ")
+        }
+        
+        // Set up Vision-CoreML Request
+        let classificationRequest = VNCoreMLRequest(model: selectedModel, completionHandler: classificationCompleteHandler)
+        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
+        visionRequests = [classificationRequest]
+        
+        // Begin Loop to Update CoreML
+        loopCoreMLUpdate()
+        
         drawInfoLabel()
+        drawLearnMoreButton()
         setupInfoLabelTimer()
         drawSaveLocationButton()
         maybeAddMapView()
         setupSceneLocationView()
+        
+        view.bringSubview(toFront: saveLocationButton)
+    }
+    
+    func drawLearnMoreButton() {
+        learn_more_button.backgroundColor = .red
+        view.addSubview(learn_more_button)
+    }
+    
+    func loopCoreMLUpdate() {
+        // Continuously run CoreML whenever it's ready. (Preventing 'hiccups' in Frame Rate)
+        
+        dispatchQueueML.async {
+            // 1. Run Update.
+            self.updateCoreML()
+            
+            // 2. Loop this function.
+            self.loopCoreMLUpdate()
+        }
+        
+    }
+    
+    func classificationCompleteHandler(request: VNRequest, error: Error?) {
+        // Catch Errors
+        if error != nil {
+            print("Error: " + (error?.localizedDescription)!)
+            return
+        }
+        guard let observations = request.results else {
+            print("No results")
+            return
+        }
+        
+        // Get Classifications
+        let classifications = observations[0...1] // top 2 results
+            .flatMap({ $0 as? VNClassificationObservation })
+            .map({ "\($0.identifier) \(String(format:"- %.2f", $0.confidence))" })
+            .joined(separator: "\n")
+        
+        
+        DispatchQueue.main.async {
+            // Print Classifications
+            
+            // Display Debug Text on screen
+            var debugText:String = ""
+            debugText += classifications
+//            self.debugTextView.text = debugText
+            
+            // Store the latest prediction
+            var objectName:String = "…"
+            objectName = classifications.components(separatedBy: "-")[0]
+            let confidence  = classifications.components(separatedBy: "-")[1].components(separatedBy: "\n")[0]
+            objectName = objectName.components(separatedBy: ",")[0]
+            
+            
+            
+            if let confidenceValue = Double(confidence.trimmingCharacters(in: CharacterSet.whitespaces)) {
+                self.latestPrediction = (confidenceValue > 0.6)  ? objectName : ""
+                
+            }
+            
+            
+        }
+    }
+    
+    func updateCoreML() {
+        ///////////////////////////
+        // Get Camera Image as RGB
+        let pixbuff : CVPixelBuffer? = (sceneLocationView.session.currentFrame?.capturedImage)
+        if pixbuff == nil { return }
+        let ciImage = CIImage(cvPixelBuffer: pixbuff!)
+        
+        
+        ///////////////////////////
+        // Prepare CoreML/Vision Request
+        let imageRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        
+        
+        do {
+            try imageRequestHandler.perform(self.visionRequests)
+        } catch {
+            print(error)
+        }
+        
+    }
+
+    func createNewBubbleParentNode(_ text : String) -> SCNNode {
+        // Warning: Creating 3D Text is susceptible to crashing. To reduce chances of crashing; reduce number of polygons, letters, smoothness, etc.
+        
+        // TEXT BILLBOARD CONSTRAINT
+        let billboardConstraint = SCNBillboardConstraint()
+        billboardConstraint.freeAxes = SCNBillboardAxis.Y
+        
+        // BUBBLE-TEXT
+        let bubble = SCNText(string: text, extrusionDepth: CGFloat(bubbleDepth))
+        var font = UIFont(name: "Futura", size: 0.15)
+        font = font?.withTraits(traits: .traitBold)
+        bubble.font = font
+        bubble.alignmentMode = kCAAlignmentCenter
+        bubble.firstMaterial?.diffuse.contents = UIColor.orange
+        bubble.firstMaterial?.specular.contents = UIColor.white
+        bubble.firstMaterial?.isDoubleSided = true
+        // bubble.flatness // setting this too low can cause crashes.
+        bubble.chamferRadius = CGFloat(bubbleDepth)
+        
+        // BUBBLE NODE
+        let (minBound, maxBound) = bubble.boundingBox
+        let bubbleNode = SCNNode(geometry: bubble)
+        // Centre Node - to Centre-Bottom point
+        bubbleNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, bubbleDepth/2)
+        // Reduce default text size
+        bubbleNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+        
+        // CENTRE POINT NODE
+        let sphere = SCNSphere(radius: 0.005)
+        sphere.firstMaterial?.diffuse.contents = UIColor.cyan
+        let sphereNode = SCNNode(geometry: sphere)
+        
+        // BUBBLE PARENT NODE
+        let bubbleNodeParent = SCNNode()
+        bubbleNodeParent.addChildNode(bubbleNode)
+        bubbleNodeParent.addChildNode(sphereNode)
+        bubbleNodeParent.constraints = [billboardConstraint]
+        
+        
+        return bubbleNodeParent
+    }
+    
+    
+    func clearView(){
+        sceneLocationView.scene.rootNode.enumerateChildNodes { (node, stop) -> Void in
+            node.removeFromParentNode()
+        }
+    }
+    
+    // MARK: - Interaction
+    @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
+        // HIT TEST : REAL WORLD
+        // Get Screen Centre
+        let screenCentre : CGPoint = CGPoint(x: self.sceneLocationView.bounds.midX, y: self.sceneLocationView.bounds.midY)
+        
+        let arHitTestResults : [ARHitTestResult] = sceneLocationView.hitTest(screenCentre, types: [.featurePoint]) // Alternatively, we could use '.existingPlaneUsingExtent' for more grounded hit-test-points.
+        
+        if let closestResult = arHitTestResults.first {
+            // Get Coordinates of HitTest
+            
+            
+            let transform : matrix_float4x4 = closestResult.worldTransform
+            let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+            
+            
+            self.clearView()
+            
+            // Create 3D Text
+            if latestPrediction.count > 0{
+                self.learn_more_button.isHidden = false
+                let node : SCNNode = createNewBubbleParentNode(latestPrediction)
+                sceneLocationView.scene.rootNode.addChildNode(node)
+                
+                
+                node.position = worldCoord
+            }
+        }
     }
     
     func drawInfoLabel() {
@@ -115,7 +310,7 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
     
     @objc func saveLocation() {
         
-        if !hasSavedLocation {
+//        if !hasSavedLocation {
             removeAllNodesFromScene()
             
             let image = UIImage(named: "pin")!
@@ -130,7 +325,7 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
             
             nodesInScene.append(carLocationNode)
             hasSavedLocation = true
-        }
+//        }
     }
     
     func removeAllNodesFromScene() {
@@ -193,7 +388,7 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
     func layoutFindCarSubviews() {
         sceneLocationView.frame = CGRect(
             x: 0,
-            y: 0,
+            y: 50,
             width: self.view.frame.size.width,
             height: self.view.frame.size.height)
         
@@ -215,6 +410,11 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
                                           y: 0,
                                           width: self.view.frame.size.width/4,
                                           height: self.view.frame.size.height/8)
+        
+        learn_more_button.frame = CGRect(x: self.view.frame.size.width/2,
+                                         y: self.view.frame.size.height,
+                                         width: self.view.frame.size.width/5,
+                                         height: self.view.frame.size.height/8)
     }
 
     @objc func updateUserLocation() {
@@ -381,3 +581,13 @@ extension UIView {
         return recursiveSubviews
     }
 }
+
+
+extension UIFont {
+    // Based on: https://stackoverflow.com/questions/4713236/how-do-i-set-bold-and-italic-on-uilabel-of-iphone-ipad
+    func withTraits(traits:UIFontDescriptorSymbolicTraits...) -> UIFont {
+        let descriptor = self.fontDescriptor.withSymbolicTraits(UIFontDescriptorSymbolicTraits(traits))
+        return UIFont(descriptor: descriptor!, size: 0)
+    }
+}
+
